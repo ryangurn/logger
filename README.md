71e552a3b4aada5bf0eb83d7f6257adc375f732b
Ryan Gurnick <ryangurnick@gmail.com>
2021-01-18 13:37:42 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# logger
+
+## functionality
+1. act as a central repository for all non-core logs
+2. connect to oauth2 provider (auther)
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('laravelpassport')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('laravelpassport')->user();
+
+    // $user->token
+});
